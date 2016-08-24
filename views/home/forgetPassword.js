38a1474b1fd42535c325958f50f64efe71981e01
chengqian98026 <272063308@qq.com>
2016-08-24 11:42:26 +0800

    Update forgetPassword.js

--- views/home/forgetPassword.js
+++ views/home/forgetPassword.js
@@ -3,16 +3,19 @@
  */
 angular.module("myapp").controller("forgetPasswordCtrl",function($scope,$interval){
     $scope.codetime = "获取验证码";
-    $scope.time = 60;
+    
     var timer = null;
 
     $scope.yanzhengma=function(){
+        if(timer!=null)return false;
+        $scope.time = 60;
         timer = $interval(function(){
             $scope.time = $scope.time - 1;
             $scope.codetime = $scope.time+"秒后点击重发";
             if($scope.time === 0) {
                 $scope.codetime = "获取验证码";
                 $interval.cancel(timer);
+                timer = null;
             }
         }, 1000);
     }
